36942a7263e768e8b035a8f7fea9e3734ed6ce9f
MKbilal <mxbilal@ualr.edu>
2022-08-26 03:19:59 +0000

    Time1

---
+++ 01.05 Previous and Next.py
@@ -0,0 +1,5 @@
+x = int(input('Enter your first number: '))
+
+print('The next number for the number {} is {}.' .format(x, x+1))
+
+print('The previous number for the number {} is {}.' .format(x, x-1))
---
+++ 01.06 Two Timestamps.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+# First Timestamp
+# Enter Hours: 6 
+# Enter Minutes: 1
+# Enter Seconds: 30
+# Second Timestamp
+# Enter Hours: 6
+# Enter Minutes: 2
+# Enter Seconds: 10
+# 40
