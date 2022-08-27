8c932bc2eb67ec1b93971257c0e8a7a4f67d36b1
MKbilal <mxbilal@ualr.edu>
2022-08-27 00:08:03 +0000

    Time

--- 01.06 Two Timestamps.py
+++ 01.06 Two Timestamps.py
@@ -1,18 +1,16 @@
+print('First Timestamp')
+x = int(input('Enter Hour number: '))
+y = int(input('Enter Minutes: '))
+z = int(input('Enter seconds: '))
 
+print('Second Timestamp')
+a = int(input('Enter Hour number: '))
+b = int(input('Enter Minutes: '))
+c = int(input('Enter seconds: '))
 
+second_1 = (x * 3600 + y * 60 + z)
+second_2 = (a * 3600 + b * 60 + c)
 
+total_second = second_1 - second_2
 
-
-
-
-
-
-# First Timestamp
-# Enter Hours: 6 
-# Enter Minutes: 1
-# Enter Seconds: 30
-# Second Timestamp
-# Enter Hours: 6
-# Enter Minutes: 2
-# Enter Seconds: 10
-# 40
+print(abs(total_second))
