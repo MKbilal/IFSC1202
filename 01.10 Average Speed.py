a30caef253631f4b48e19f707c071ae619a8f312
MKbilal <mxbilal@ualr.edu>
2022-08-29 15:05:07 +0000

    01.10 Average Speed

--- 01.10 Average Speed.py
+++ 01.10 Average Speed.py
@@ -2,4 +2,10 @@ a = int(input('Enter Length of Race in Kilometer: '))
 b = int(input('Enter Minutes: '))
 c = int(input('Enter seconds: '))
 
-print((a/((b*60+c)/3600))/1.61)
+t = (b*60+c) 
+y = (t/3600)
+u =(a/y)
+o = (u/1.61)
+print(o)
+
+
