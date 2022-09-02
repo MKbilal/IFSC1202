b09fc77ca92589d0d761d1f4fff2714537d9cecf
MKbilal <mxbilal@ualr.edu>
2022-09-02 03:51:25 +0000

    Sum of digit

--- 02.06 Sum of Digits.py
+++ 02.06 Sum of Digits.py
@@ -1,7 +1,8 @@
-x= int(input("Enter a 3 Digit Number: "))
+x = int(input("Enter a 3 Digit Number: "))
 y = 0
-for x in range(0,3):
+for z in range(0,3):
     y += x % 10
     x = x // 10
 
 print("Sum of Digits: {}".format(y))
+
