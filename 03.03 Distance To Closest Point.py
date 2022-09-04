8d13f2e45e4734109ee39260357d5c29e2cb89e8
MKbilal <mxbilal@ualr.edu>
2022-09-04 23:32:51 +0000

    Distance to closest

---
+++ 03.03 Distance To Closest Point.py
@@ -0,0 +1,15 @@
+A = int(input("Enter point A: "))
+B = int(input("Enter point B: "))
+C = int(input("Enter point C: "))
+
+D = abs(B - A)
+E = abs(C - A)
+
+if (D > E):
+    print(E)
+
+
+else:
+    print(D)
+
+
