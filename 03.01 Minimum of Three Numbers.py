5ee2823d92a4a940392fcb4291489c1a6169bcdb
MKbilal <mxbilal@ualr.edu>
2022-09-05 00:10:35 +0000

    close distance

--- 03.01 Minimum of Three Numbers.py
+++ 03.01 Minimum of Three Numbers.py
@@ -1,11 +1,16 @@
 a = int(input('Enter first number: '))
 b = int(input('Enter second number: '))
 c = int(input('Enter third number: '))
+d = int(input('Enter fourth number: '))
 
-if a < b & a < c :
+if a < b & a < c & a < d:  #a is smaller than b and c
     x = a
-elif b < c:
+elif b < c & b < a & b < d:     # b is smaller than c 
     x = b
-else:
+elif c < d & c < a & c < d: # c is smaller d
     x = c
+else:
+    x = d
 print(x)
+
+
