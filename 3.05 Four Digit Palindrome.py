47ce662b8b3d5e6a525216288ae2311353ecb4a6
MKbilal <111479037+MKbilal@users.noreply.github.com>
2022-09-19 09:14:47 -0500

    Update 3.05 Four Digit Palindrome.py

--- 3.05 Four Digit Palindrome.py
+++ 3.05 Four Digit Palindrome.py
@@ -1,3 +1,7 @@
+#A palindrome is a number which reads the same when read forward as it it does when read backward.
+#Given a four-digit integer, print "YES" if it's a palindrome and print "NO" otherwise.
+#Hint: put the units, tens, hundreds, and thousands in separate variables. Do not use any string functions.
+
 x = int(input('Enter a number: '))
 
 a = x % 10
