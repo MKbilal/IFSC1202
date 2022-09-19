581597fb5c01ee7064ec8dc0b5c2a52c0c3ac687
MKbilal <111479037+MKbilal@users.noreply.github.com>
2022-09-19 09:18:32 -0500

    Update 03.08 Days In Month.py

--- 03.08 Days In Month.py
+++ 03.08 Days In Month.py
@@ -1,3 +1,7 @@
+#Given a month - an integer from 1 (January) to 12 (December), print the number of days in the month.
+#Assume that it is a non-leap year.
+
+
 month = int(input("Enter month: "))
 
 if month in [9, 4, 6, 11]:
