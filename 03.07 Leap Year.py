9114a6c9ba7ceb72aae47c6bbbe9728a80f25361
MKbilal <111479037+MKbilal@users.noreply.github.com>
2022-09-19 09:17:45 -0500

    Update 03.07 Leap Year.py

--- 03.07 Leap Year.py
+++ 03.07 Leap Year.py
@@ -1,3 +1,9 @@
+#Prompt for a 4 digit year'
+#If the year is a leap year, print "LEAP YEAR", otherwise print "COMMON YEAR".
+#The rules in Gregorian calendar are as follows:
+#A year is a leap year if its number is exactly divisible by 4 and is not exactly divisible by 100
+#A year is always a leap year if its number is exactly divisible by 400
+
 n = int(input("Enter year: "))
 if n % 400 == 0 or n % 100 != 0 and n % 4 == 0:
   print("LEAP YEAR")
