f074801303ff36363c2a55d338593c7c602fbc2a
MKbilal <111479037+MKbilal@users.noreply.github.com>
2022-09-19 09:13:27 -0500

    Update 03.04 Digits In Ascending Order.py

--- 03.04 Digits In Ascending Order.py
+++ 03.04 Digits In Ascending Order.py
@@ -1,3 +1,7 @@
+#Given a three-digit integer, print "YES" if its digits are in ascending order left to right, print "NO" otherwise.
+#Do not try to sort the number. Determine each digit and use "if" statements.
+
+
 x = int(input('Enter a number: '))
 
 a = x % 10
