8dfea335fd66ddef77ce2c43a039cb6c5bc3e97d
MKbilal <mxbilal@ualr.edu>
2022-11-01 04:46:23 +0000

    Cars

--- 10.03 Cars.py
+++ 10.03 Cars.py
@@ -1,36 +1,37 @@
 class Car:
-    def _init_(self, Year, Make): #constructor
-        self.Year = Year
-        self.make = Make
-        self.Sspeed = 0
-
-    def Accelerate(self, value):
-        self.Speed = self.speed + value
-
-    def Brake(self, value):
-        self.Speed = self.Speed - value
-        if self.Speed < 0: #if speed goes below 0, assign 0 to speed
-            self.Speed = 0
-
-def changespeed(x):
-    if x > 0:
-        cari.Accelerate(x)
-    else:
-        cari.Brake(-x) #-x as the function accepts positive deceleration values
-
-feopen("10.03 Cars.txt", "r") #open file car.txt in reading mode
-
-fl =f.readlines() #readlines() reads the file untill EOF is encountered, and returns a List containing the Lines
-l1 = fl{o].split(” *) #read the first record which contains the car year and the make
-car1 = Car(11[0],11[1]) #instantiate a car object with the first record extracted from the file
-
-for x in fl[1:]: #loop through the Lines extracted from car.txt, skip first Line
-    changespeed( float (x))
-
-print(f'vear : {car1.Year}')
-
-print(f'Make : {car1.Make}')
-
-print(f'Speed : {car1.Speed}')
-
-f.close() #close file once we are done working with it, always a good practice
+        def __init__(self, year, make):
+                self.Year = year
+                self.Make = make
+                self.Speed = 0
+        def Accelerate(self,x):
+                self.Speed = self.Speed + x
+        def Brake(self, x):
+                if self.Speed < x:
+                        x = x - self.Speed
+                self.Speed = self.Speed - x
+def changespeed(speed, c):
+        print("\nChange   Speed")
+        for i in speed:
+                print(i, end = "\t    ")
+                if i > 0:
+                        c.Accelerate(i)
+                else:
+                        i=i*(-1)
+                        c.Brake(i)
+                print(c.Speed)
+words = [ ]
+speed = [ ]
+with open("10.03 Cars.txt") as file:
+        line = file.read()
+        lines = line.split("\n")
+        word = lines[0].split(", ")
+        for k in word:
+                words.append(k)
+        yr = words[0]
+        mk = words[1]
+        for k in range(1,len(lines)):
+                speed.append(int(lines[k])) 
+c=Car(yr, mk)
+print("Make: ", c.Make)
+print("Year: ", c.Year)
+changespeed(speed, c)
