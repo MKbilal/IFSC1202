d30ec9ac7c6ed6eea88ff82fdaff7228a97d9e07
MKbilal <mxbilal@ualr.edu>
2022-11-09 02:59:26 +0000

    retail item

---
+++ 11.01 Retail Item.py
@@ -0,0 +1,34 @@
+class RetailItem:
+    def __init__(self, desc = '', units = 0, price = 0.0):
+        self.description = desc
+        self.unitsOnHand = units
+        self.price = price
+    def InventoryValue(self):
+        return self.unitsOnHand * self.price
+def main():
+    file = '11.01 Inventory.txt'
+    items=[]
+    file_in = open(file)
+    for line in file_in:
+        data = line.strip().split(',')
+        name, units, price = data[0].strip(), int(data[1].strip()), float(data[2].strip())
+        retail_item = RetailItem(name, units, price)
+        items.append(retail_item)
+    file_in.close()
+    print('{:>12}{:>15}{:>15}{:>25}'.format('Description',' Units On Hand','Price','Inventory Value'))
+    for item in items:
+        print(f'{item.description:>12}{item.unitsOnHand:>15}{item.price:>15.2f}{item.InventoryValue():>25.2f}')
+    print("")
+    file = "11.01 InventoryUpdate.txt"
+    file_in = open(file)
+    for line in file_in:
+        data = line.strip().split(',')
+        name, price = data[0].strip(), float(data[1].strip())
+        for item in items:
+            if (name == item.description):
+                item.price = price
+    file_in.close()
+    print('{:>12}{:>15}{:>15}{:>25}'.format('Description',' Units On Hand','Price','Inventory Value'))
+    for item in items:
+        print(f'{item.description:>12}{item.unitsOnHand:>15}{item.price:>15.2f}{item.InventoryValue():>25.2f}')
+main()
